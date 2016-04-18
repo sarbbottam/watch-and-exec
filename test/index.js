81b0b9b8ad4d069fb15b48709917b10a272c93cf
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-04-18 14:50:27 -0700

    fix(test): execution order and assertion

--- test/index.js
+++ test/index.js
@@ -12,18 +12,17 @@ exec(`rm -rf ./test/log`);
 
 exec(`nohup ./src/index.js -d=${dir} -c=${command} > /dev/null 2>&1 &`, function(error, stdout, stderr) {
   assert.equal(error, null);
+
+  setTimeout(() => {
+    exec(`touch ./test/fixtures/foo`);
+    setTimeout(() => {
+      exec('ps -ef | grep ' + dir + ' | grep -v grep | awk \'{print $2}\' | xargs kill -9', function(error, stdout, stderr) {
+        const log = fs.readFileSync(path.join(process.cwd(), './test/log'), 'utf8');
+        assert.notEqual(log.indexOf('foo'), -1);
+        console.log('All the test have passed successfully');
+        exec(`rm -rf ./test/fixtures`);
+        exec(`rm -rf ./test/log`);
+      });
+    }, 100)
+  }, 1000)
 });
-
-setTimeout(() => {
-  exec(`touch ./test/fixtures/foo_{1..2}`);
-}, 200)
-
-setTimeout(() => {
-  exec('ps -ef | grep ' + dir + ' | grep -v grep | awk \'{print $2}\' | xargs kill -9', function(error, stdout, stderr) {
-    const log = fs.readFileSync(path.join(process.cwd(), './test/log'), 'utf8');
-    assert.equal(log, 'foo_1\nfoo_2\n');
-    console.log('All the test have passed successfully');
-    exec(`rm -rf ./test/fixtures`);
-    exec(`rm -rf ./test/log`);
-  });
-}, 300)
