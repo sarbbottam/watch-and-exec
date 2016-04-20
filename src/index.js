e0912d31422d1f57cc3350aa114c94ec9ea089eb
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-04-20 13:53:15 -0700

    fix(lint): using xo for linting

--- src/index.js
+++ src/index.js
@@ -24,6 +24,6 @@ const argv = require('yargs')
   .demand(['d', 'c'])
   .strict()
   .alias(options)
-  .argv
+  .argv;
 
 watch(argv.d, argv.c);
--- test/index.js
+++ test/index.js
@@ -5,24 +5,24 @@ const path = require('path');
 const exec = require('child_process').exec;
 
 const dir = path.join(process.cwd(), 'test/fixtures');
-const command = `'${path.join(process.cwd(), './test/logger')} sarbbottam'` ;
+const command = `'${path.join(process.cwd(), './test/logger')} sarbbottam'`;
 
 exec(`mkdir -p ./test/fixtures`);
 exec(`rm -rf ./test/log`);
 
-exec(`nohup ./src/index.js -d=${dir} -c=${command} > /dev/null 2>&1 &`, function(error, stdout, stderr) {
+exec(`nohup ./src/index.js -d=${dir} -c=${command} > /dev/null 2>&1 &`, error => {
   assert.equal(error, null);
 
   setTimeout(() => {
     exec(`touch ./test/fixtures/foo`);
     setTimeout(() => {
-      exec('ps -ef | grep ' + dir + ' | grep -v grep | awk \'{print $2}\' | xargs kill -9', function(error, stdout, stderr) {
+      exec(`ps -ef | grep ${dir} | grep -v grep | awk '{print $2}' | xargs kill -9`, () => {
         const log = fs.readFileSync(path.join(process.cwd(), './test/log'), 'utf8');
         assert.notEqual(log.indexOf('sarbbottam'), -1);
         console.log('All the test have passed successfully');
         exec(`rm -rf ./test/fixtures`);
         exec(`rm -rf ./test/log`);
       });
-    }, 100)
-  }, 1000)
+    }, 100);
+  }, 1000);
 });
